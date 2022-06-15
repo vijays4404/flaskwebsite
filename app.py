4b78aacd04955139c159ac2a5458d2e558be1633
Vijay S <82169023+vijays4404@users.noreply.github.com>
2022-06-15 09:19:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+#importing Flask
+from flask import Flask
+#importing views from view python file
+from view import views
+#storing Flask instance to a variable
+app=Flask(__name__)
+app.register_blueprint(views,url_prefix="/views")
+
+
+
+if __name__=='__main__':
+    app.run()
+    
